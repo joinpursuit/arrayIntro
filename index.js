7cfc7ada8c4e975138e4720ce1cbdaf110495a20
Corey Ladovsky <coreyskymusic@gmail.com>
2019-10-06 16:45:06 -0400

    added concat, equality, muli d

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // let fruits = ["blueberry", "banana", "kiwi", "cherry"] // GOOD TO HAVE THE SAME TYPE OF DATA! 
 // fruits[0] = "rasberry"
-let nums = [13, 12, 14, 56]
+// let nums = [13, 12, 14, 56]
 
 // console.log(fruits.slice(2, 2))
 // console.log(fruits.join(" "))
@@ -25,17 +25,46 @@ let nums = [13, 12, 14, 56]
 // fruits.push("snake") // (blueberry, balloon, kiwi), cow, snake
 // console.log(fruits)
 
-let str = "My dog has fleas";
-str = str.slice(3); // "dog has fleas"
-let arr = str.split(" "); // [ dog, has, fleas]
-arr.slice(5); // []
-console.log(arr.length); // 3
-arr[0] = "my cat"; // ["my cat", has, fleas]
-arr.push(["Hello", "dolly"]); // ["my cat", has, fleas, [hello, dolly]]
-arr.push(1, 2, 3); // ["my cat", has, fleas, [hello, dolly], 1, 2, 3]
-arr.pop(); // ["my cat", has, fleas, [hello, dolly], 1, 2]
-arr.unshift("Good luck"); // ["good luck","my cat", has, fleas, [hello, dolly], 1, 2]
-arr = arr.slice(1); // ["my cat", has, fleas, [hello, dolly], 1, 2]
-console.log(arr);
-console.log(str) // dog has fleas
-console.log(arr.length) // 6
+// let str = "My dog has fleas";
+// str = str.slice(3); // "dog has fleas"
+// let arr = str.split(" "); // [ dog, has, fleas]
+// arr.slice(5); // []
+// console.log(arr.length); // 3
+// arr[0] = "my cat"; // ["my cat", has, fleas]
+// arr.push(["Hello", "dolly"]); // ["my cat", has, fleas, [hello, dolly]]
+// arr.push(1, 2, 3); // ["my cat", has, fleas, [hello, dolly], 1, 2, 3]
+// arr.pop(); // ["my cat", has, fleas, [hello, dolly], 1, 2]
+// arr.unshift("Good luck"); // ["good luck","my cat", has, fleas, [hello, dolly], 1, 2]
+// arr = arr.slice(1); // ["my cat", has, fleas, [hello, dolly], 1, 2]
+// console.log(arr);
+// console.log(str) // dog has fleas
+// console.log(arr.length) //
+
+// let arr = [1, 2, 3];
+// let secondArr = [4, 5, 6];
+// console.log(arr.concat(secondArr).concat([7, 8, 9])) 
+// console.log(arr)
+
+// let arr = [1, 2, 3];
+// let secondArr = [1, 2, 3];
+// console.log(arr === secondArr); // Cannot check for array equality;
+// let double = arr ; 
+// arr.push(4)
+// console.log(double === arr)
+// console.log(double)
+
+let mtx = [
+            [1, 2, 3],
+            [4, 5, 6], 
+            [7, 8, 9]
+        ]
+
+        // console.log(mtx[2][0])
+
+        let test = [1, 2, [3, 4, [5, [6, "string"], 7, 8], 9]]
+          // Give me the letter n
+          //[ 3, 4, [5, [6, "string"], 7, 8], 9]
+        //   (5, [6, "string"], 7, 8)
+        // ];
+        console.log(test[2][2][1][1][4])
+// console.log(test.length)
